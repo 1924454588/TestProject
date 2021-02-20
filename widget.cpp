867d239d5489c9f09578b66f44de920a98896c01
unknown <1924454588@qq.com>
2021-02-20 10:31:34 +0800

    初始化

---
+++ widget.cpp
@@ -0,0 +1,25 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    setWindowFlag(Qt::FramelessWindowHint);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_toolButton_3_clicked()
+{
+    this->close();
+}
+
+void Widget::on_toolButton_2_clicked()
+{
+    this->showMaximized();
+}
